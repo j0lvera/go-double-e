9209c402dae3f6d95a67c1536747fe7a09b1b3bc
Juan Olvera <juan@jolvera.dev>
2024-12-25 21:32:38 -0600

    feat: add general errors

---
+++ internal/server/errorMsgs.go
@@ -0,0 +1,10 @@
+package server
+
+// General error message to return to the client
+const (
+	ErrInternalServerError = "Internal Server Error"
+	ErrInvalidRequest      = "Invalid request"
+	ErrUnauthorized        = "Unauthorized"
+	ErrForbidden           = "Forbidden"
+	ErrNotFound            = "Not Found"
+)
