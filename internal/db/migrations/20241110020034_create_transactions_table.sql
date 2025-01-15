60914b246caf5321fb242df9f7bc05bb11a9c0c1
Juan Olvera <juan@jolvera.dev>
2025-01-15 10:28:02 -0600

    refactor: remove entries, account balances migrations

--- internal/db/migrations/20241110020034_create_transactions_table.sql
+++ internal/db/migrations/20241110020034_create_transactions_table.sql
@@ -4,23 +4,27 @@ create type transaction_status as enum ('pending', 'posted');
 
 create table transactions
 (
-    id          bigint generated always as identity primary key,
-    uuid        text               not null default nanoid(10),
+    id                bigint generated always as identity primary key,
+    uuid              text               not null default nanoid(10),
 
-    created_at  timestamptz        not null default current_timestamp,
-    updated_at  timestamptz        not null default current_timestamp,
+    created_at        timestamptz        not null default current_timestamp,
+    updated_at        timestamptz        not null default current_timestamp,
 
-    status      transaction_status not null default 'pending',
+    amount            bigint             not null default 0,
+    status            transaction_status not null default 'pending',
     -- track when a transaction was posted
-    date        date,
-    description text,
-    metadata    jsonb,
+    date              date,
+    description       text,
+    metadata          jsonb,
 
+    credit_account_id bigint             not null references accounts (id) on delete cascade,
+    debit_account_id  bigint             not null references accounts (id) on delete cascade,
     -- links back to ledger and user for easy querying
-    ledger_id   bigint             not null references ledgers (id) on delete cascade,
+    ledger_id         bigint             not null references ledgers (id) on delete cascade,
 
     -- constraints
     constraint transactions_uuid_unique unique (uuid),
+    constraint transactions_amount_positive check (amount >= 0),
     constraint transactions_description_length_check check (char_length(description) < 255)
 );
 
